c58e425e5875d1a6e7fc25cd4bd60984986a1856
bangtony <74534308+bangtony2@users.noreply.github.com>
2025-06-11 18:40:50 +0300

    Update set_time_date_russian.bat

--- settings/set_time_date_russian.bat
+++ settings/set_time_date_russian.bat
@@ -7,3 +7,5 @@ reg add "HKCU\Control Panel\International" /v sLongDate /t REG_SZ /d "dd MMMM yy
 reg add "HKCU\Control Panel\International" /v sDate /t REG_SZ /d "." /f
 reg add "HKCU\Control Panel\International" /v Locale /t REG_SZ /d "00000419" /f
 reg add "HKCU\Control Panel\International" /v LocaleName /t REG_SZ /d "ru-RU" /f
+taskkill /f /im explorer.exe
+start explorer.exe
